82a0e97c0cd702de85af75b8f572389cbeb8b07a
Mike Sakata <mgsakata@gmail.com>
2012-11-12 21:01:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artificiallysmart::Application.config.secret_token = '22229e3696c8c5a803cd7c8a91087ae7bcf7bd1f1578e91452f78ff8e961f6a228011356d38ade07b063a63747e7ad378b7769c5526db96f1794a93e396c5bd1'
