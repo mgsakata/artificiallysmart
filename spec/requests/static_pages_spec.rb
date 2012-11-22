4237f969474e2fd66c8db0e5e701b566676d6759
Mike Sakata <mgsakata@gmail.com>
2012-11-21 22:27:31 -0500

    Added basic User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Artificially Smart'" do
-      visit '/static_pages/home'
+      visit '/'
       page.should have_content('Artificially Smart')
     end
   end
@@ -13,7 +13,7 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       page.should have_content('About Us')
     end
   end
