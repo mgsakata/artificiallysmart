6fb67e884c14744d7e7417ce801b83cfbf3592a3
Mike Sakata <mgsakata@gmail.com>
2013-12-03 23:16:24 -0500

    New version of artificially smart!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Artificiallysmart::Application.config.secret_token = '22229e3696c8c5a803cd7c8a91087ae7bcf7bd1f1578e91452f78ff8e961f6a228011356d38ade07b063a63747e7ad378b7769c5526db96f1794a93e396c5bd1'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Artificiallysmart::Application.config.secret_key_base = secure_token
