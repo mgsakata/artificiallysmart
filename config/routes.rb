11f97f93312f5b73d0bd115c2afcf16c8ba7328e
Mike Sakata <mgsakata@gmail.com>
2013-12-03 23:25:42 -0500

    Added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Artificiallysmart::Application.routes.draw do
 
-  root  'static_pages#home'
+  root 'static_pages#home'
+  get "static_pages/help"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
